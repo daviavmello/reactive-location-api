e012fac4c3a4851f70099820066996ff13006573
Davi Mello <davimello1994@gmail.com>
2023-10-04 18:25:37 -0400

    Removing unused import

--- src/main/java/com/davimello/reactivelocationservice/api/PlaceRequest.java
+++ src/main/java/com/davimello/reactivelocationservice/api/PlaceRequest.java
@@ -1,8 +1,5 @@
 package com.davimello.reactivelocationservice.api;
 
 import jakarta.validation.constraints.NotBlank;
-
-import java.time.LocalDateTime;
-
 public record PlaceRequest(@NotBlank String name, @NotBlank String city, @NotBlank String state) {
 }
